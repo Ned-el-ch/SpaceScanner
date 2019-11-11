036cf5450b874fd7b372f331250080526baaab39
Ned-el-ch <nedelchev.niki@gmail.com>
2019-11-11 17:09:52 +0000

    basic styling before bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
     # resources :users
 
     # get '/', to: "application#home"
-    get '/trips', to: "application#trips"
-    get '/account', to: "application#account"
+
 
     resources :users, only: [:new, :create]
     get 'login', to: 'sessions#new'
@@ -18,6 +17,9 @@ Rails.application.routes.draw do
     get 'welcome', to: 'sessions#welcome'
     get '/', to: 'sessions#welcome'
     post 'logout', to: 'sessions#logout'
+
+    get '/trips', to: "application#trips"
+    get '/account', to: "application#account"
 
     get 'authorized', to: 'sessions#page_requires_login'
 
